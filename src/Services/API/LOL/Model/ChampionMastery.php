dfe080d3b530e5ab8ce703e83ae5e966d850f1f3
Zeggiim <lilian.dorazio@hotmail.fr>
2022-01-25 19:49:06 +0100

    Champion

--- src/Services/API/LOL/Model/ChampionMastery.php
+++ src/Services/API/LOL/Model/ChampionMastery.php
@@ -1,10 +1,127 @@
 <?php
 
-
 namespace App\Services\API\LOL\Model;
-
 
 class ChampionMastery
 {
+    private int $championPointsUntilNextLevel;
+    private bool $chestGranted;
+    private int $championId;
+    private int $lastPlayTime;
+    private int $championLevel;
+    private string $summonerId;
+    private int $championPoints;
+    private int $tokensEarned;
 
+    /**
+     * @return int
+     */
+    public function getChampionPointsUntilNextLevel(): int
+    {
+        return $this->championPointsUntilNextLevel;
+    }
+
+    /**
+     * @param int $championPointsUntilNextLevel
+     * @return ChampionMastery
+     */
+    public function setChampionPointsUntilNextLevel(int $championPointsUntilNextLevel): ChampionMastery
+    {
+        $this->championPointsUntilNextLevel = $championPointsUntilNextLevel;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isChestGranted(): bool
+    {
+        return $this->chestGranted;
+    }
+
+    /**
+     * @param bool $chestGranted
+     * @return ChampionMastery
+     */
+    public function setChestGranted(bool $chestGranted): ChampionMastery
+    {
+        $this->chestGranted = $chestGranted;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getChampionId(): int
+    {
+        return $this->championId;
+    }
+
+    /**
+     * @param int $championId
+     * @return ChampionMastery
+     */
+    public function setChampionId(int $championId): ChampionMastery
+    {
+        $this->championId = $championId;
+        return $this;
+    }
+
+    public function getLastPlayTime(): int
+    {
+        return $this->lastPlayTime;
+    }
+
+    public function setLastPlayTime(int $lastPlayTime): ChampionMastery
+    {
+        $this->lastPlayTime = $lastPlayTime;
+        return $this;
+    }
+
+    public function getChampionLevel(): int
+    {
+        return $this->championLevel;
+    }
+
+    public function setChampionLevel(int $championLevel): ChampionMastery
+    {
+        $this->championLevel = $championLevel;
+        return $this;
+    }
+
+    public function getSummonerId(): string
+    {
+        return $this->summonerId;
+    }
+
+    public function setSummonerId(string $summonerId): ChampionMastery
+    {
+        $this->summonerId = $summonerId;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getChampionPoints(): int
+    {
+        return $this->championPoints;
+    }
+
+    public function setChampionPoints(int $championPoints): ChampionMastery
+    {
+        $this->championPoints = $championPoints;
+        return $this;
+    }
+
+    public function getTokensEarned(): int
+    {
+        return $this->tokensEarned;
+    }
+
+    public function setTokensEarned(int $tokensEarned): ChampionMastery
+    {
+        $this->tokensEarned = $tokensEarned;
+        return $this;
+    }
 }
--- src/Services/API/LOL/Model/ChampionRotation.php
+++ src/Services/API/LOL/Model/ChampionRotation.php
@@ -1,10 +1,67 @@
 <?php
 
-
 namespace App\Services\API\LOL\Model;
-
 
 class ChampionRotation
 {
+    private int $maxNewPlayerLevel;
 
+    /**
+     * @var array <array-key,int>
+     */
+    private array $freeChampionIdsForNewPlayers;
+    /**
+     * @var array <array-key,int>
+     */
+    private array $freeChampionIds;
+
+    public function getMaxNewPlayerLevel(): int
+    {
+        return $this->maxNewPlayerLevel;
+    }
+
+    public function setMaxNewPlayerLevel(int $maxNewPlayerLevel): self
+    {
+        $this->maxNewPlayerLevel = $maxNewPlayerLevel;
+
+        return $this;
+    }
+
+    /**
+     * @return array <array-key,int>
+     */
+    public function getFreeChampionIdsForNewPlayers(): array
+    {
+        return $this->freeChampionIdsForNewPlayers;
+    }
+
+    /**
+     * @param array<array-key,int> $freeChampionIdsForNewPlayers
+     * @return ChampionRotation
+     */
+    public function setFreeChampionIdsForNewPlayers(array $freeChampionIdsForNewPlayers): self
+    {
+        $this->freeChampionIdsForNewPlayers = $freeChampionIdsForNewPlayers;
+
+        return $this;
+    }
+
+    /**
+     * @return array <array-key,int>
+     */
+    public function getFreeChampionIds(): array
+    {
+        return $this->freeChampionIds;
+    }
+
+    /**
+     * @param array<array-key,int> $freeChampionIds
+     * @return ChampionRotation
+     */
+    public function setFreeChampionIds(array $freeChampionIds): self
+    {
+        $this->freeChampionIds = $freeChampionIds;
+
+        return $this;
+    }
 }
