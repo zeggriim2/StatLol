f4bd6580c332a4e4bf3c0691fbe7c56dfcbfa422
Zeggiim <lilian.dorazio@hotmail.fr>
2022-01-25 19:49:37 +0100

    Base API

--- src/Services/API/LOL/BaseApi.php
+++ src/Services/API/LOL/BaseApi.php
@@ -8,21 +8,23 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 class BaseApi
 {
     public string $apiKey;
+    public string $platform;
     public HttpClientInterface $client;
-
 
     public function __construct(
         string $apiKey,
+        string $platform,
         HttpClientInterface $client
     ) {
-        $this->apiKey = $apiKey;
-        $this->client = $client;
+        $this->apiKey   = $apiKey;
+        $this->platform = $platform;
+        $this->client   = $client;
     }
 
     /**
      *
      * @param string $url
-     * @param array<string> $params
+     * @param array<string,string> $params
      * @return string
      */
     public function constructUrl(string $url, array $params): string
@@ -36,18 +38,22 @@ class BaseApi
     /**
      * @param string $url
      * @param string $method
-     * @param array<string> $options
-     * @return array<string>|null
+     * @param array<string,string|array<string|string>> $options
+     * @param string $return
+     * @return array<array-key|string|int,int|string|array<array-key|string|int,int|string>>|string|null
      */
-    public function callApi(string $url, string $method = "GET", array $options = []): ?array
+    public function callApi(string $url, string $method = "GET", array $options = [], string $return = "array")
     {
         $response = $this->client->request($method, $url, $options);
         $statusCode = $response->getStatusCode();
-
         switch ($statusCode) {
             case Response::HTTP_OK:
-                return $response->toArray();
-
+                if ($return == "array") {
+                    return $response->toArray();
+                } else {
+                    return $response->getContent();
+                }
+            case Response::HTTP_NOT_FOUND:
             default:
                 return null;
         }
