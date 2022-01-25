f963faa1c31ce14b8aacc1ee010cef144fd56174
Zeggiim <lilian.dorazio@hotmail.fr>
2022-01-25 19:49:25 +0100

    Summoner

--- src/Services/API/LOL/SummonerApi.php
+++ src/Services/API/LOL/SummonerApi.php
@@ -8,11 +8,11 @@ use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 
 class SummonerApi
 {
-    private const URL_RACINE = "https://{region}.api.riotgames.com/lol/summoner/v4/summoners/";
+    private const URL_RACINE = "https://{platform}.api.riotgames.com/lol/summoner/v4/summoners/";
     private const URL_NAME = self::URL_RACINE . "by-name/{name}";
     private const URL_ACCOUNTID = self::URL_RACINE . "by-account/{accountId}";
     private const URL_PUUID = self::URL_RACINE . "by-puuid/{puuid}";
-    private const URL_SUMMONERID = self::URL_RACINE . "{summonerId}";
+    private const URL_SUMMONER_ID = self::URL_RACINE . "{summonerId}";
 
     private BaseApi $baseApi;
     private DenormalizerInterface $denormalizer;
@@ -27,15 +27,16 @@ class SummonerApi
 
     public function summonerBySummonerName(string $summonerName): ?Summoner
     {
+
         $url = $this->baseApi->constructUrl(self::URL_NAME, [
-            "region" => "euw1",
+            "platform" => $this->baseApi->platform,
             "name" => $summonerName
         ]);
 
-
+        /** @var array<string,int|string>|null $summoner */
         $summoner = $this->baseApi->callApi(
-            Request::METHOD_GET,
             $url,
+            Request::METHOD_GET,
             [
                 "headers" => [
                     "X-Riot-Token" => $this->baseApi->apiKey
@@ -52,13 +53,13 @@ class SummonerApi
     public function summonerByAccountId(string $accountId): ?Summoner
     {
         $url = $this->baseApi->constructUrl(self::URL_ACCOUNTID, [
-            "region" => "euw1",
+            "platform" => $this->baseApi->platform,
             "accountId" => $accountId
         ]);
-
+        /** @var array<string,int|string>|null $summoner */
         $summoner = $summoner = $this->baseApi->callApi(
-            Request::METHOD_GET,
             $url,
+            Request::METHOD_GET,
             [
                 "headers" => [
                     "X-Riot-Token" => $this->baseApi->apiKey
@@ -76,13 +77,14 @@ class SummonerApi
     public function summonerByPuuid(string $puuid): ?Summoner
     {
         $url = $this->baseApi->constructUrl(self::URL_PUUID, [
-            "region" => "euw1",
+            "platform" => $this->baseApi->platform,
             "puuid" => $puuid
         ]);
 
+        /** @var array<string,int|string>|null $summoner */
         $summoner = $summoner = $this->baseApi->callApi(
-            Request::METHOD_GET,
             $url,
+            Request::METHOD_GET,
             [
                 "headers" => [
                     "X-Riot-Token" => $this->baseApi->apiKey
@@ -99,14 +101,15 @@ class SummonerApi
 
     public function summonerBySummonerId(string $summonerId): ?Summoner
     {
-        $url = $this->baseApi->constructUrl(self::URL_SUMMONERID, [
-            "region" => "euw1",
+        $url = $this->baseApi->constructUrl(self::URL_SUMMONER_ID, [
+            "platform" => $this->baseApi->platform,
             "summonerId" => $summonerId
         ]);
 
+        /** @var array<string,int|string>|null $summoner */
         $summoner = $summoner = $this->baseApi->callApi(
-            Request::METHOD_GET,
             $url,
+            Request::METHOD_GET,
             [
                 "headers" => [
                     "X-Riot-Token" => $this->baseApi->apiKey
@@ -121,6 +124,10 @@ class SummonerApi
         return $this->denormalize($summoner);
     }
 
+    /**
+     * @param array<string,int|string> $data
+     * @return Summoner
+     */
     private function denormalize(array $data): Summoner
     {
         return $this->denormalizer->denormalize($data, Summoner::class);
