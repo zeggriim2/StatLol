dc27e126f403a0d6da61f0219cac05c82c65bba8
Zeggiim <lilian.dorazio@hotmail.fr>
2022-01-25 19:52:00 +0100

    Region

--- src/Services/API/LOL/Model/Region.php
+++ src/Services/API/LOL/Model/Region.php
@@ -1,1 +1,17 @@
 <?php
+
+namespace App\Services\API\LOL\Model;
+
+class Region
+{
+    public const BR = "br";
+    public const EUN = "eun1";
+    public const EUW = "euw1";
+    public const JP = "jp";
+    public const LA1 = "la1";
+    public const LA2 = "la2";
+    public const NA1 = "na1";
+    public const OC1 = "oc1";
+    public const RU = "ru";
+    public const TR1 = "TR1";
+}
