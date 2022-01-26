9d89e20b072f25d8e4e23151b42d21f978bf708f
Lilian <lilian.dorazio@hotmail.fr>
2022-01-26 17:29:28 +0100

    Config Tier + Division

---
+++ src/Services/API/LOL/Model/Config/Division.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Services\API\LOL\Model\Config;
+
+class Division
+{
+    public const DIVISION_I     = "I";
+    public const DIVISION_II    = "II";
+    public const DIVISION_III   = "III";
+    public const DIVISION_IV    = "IV";
+    
+
+    public const ALL_DIVISION = [
+        self::DIVISION_I,
+        self::DIVISION_II,
+        self::DIVISION_III,
+        self::DIVISION_IV,
+    ];
+}
