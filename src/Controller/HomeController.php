2c510c6ff0cfa359fcd31f6ed3cecd579809e6f8
Zeggiim <lilian.dorazio@hotmail.fr>
2022-01-27 13:08:20 +0100

    Clean Controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,18 @@
 
 namespace App\Controller;
 
-use App\Services\API\LOL\ChampionMasteryApi;
+use App\Services\API\LOL\LeagueApi;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    /**
-     * @Route("/", name="home")
-     * @param ChampionMasteryApi $championMasteryApi
-     * @return Response
-     */
-    public function home(ChampionMasteryApi $championMasteryApi): Response
+	/**
+	 * @Route("/", name="home")
+	 * @return Response
+	 */
+    public function home(): Response
     {
         return $this->render("home.html.twig");
     }
