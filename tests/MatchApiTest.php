0546e2e0e8ac2ca84b27b9c6b8937aaa237f68ed
Zeggiim <lilian.dorazio@hotmail.fr>
2022-01-31 19:51:02 +0100

    Clean

--- tests/MatchApiTest.php
+++ tests/MatchApiTest.php
@@ -2,9 +2,7 @@
 
 namespace App\Tests;
 
-use App\Services\API\LOL\ChampionRotationApi;
 use App\Services\API\LOL\MatchApi;
-use App\Services\API\LOL\Model\ChampionRotation;
 use App\Services\API\LOL\Model\Matchs;
 use App\Services\API\LOL\Model\MatchsTimeLine;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
