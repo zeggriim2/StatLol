36ba6002c93859738b24f2b51625bde0352c6e49
Zeggiim <lilian.dorazio@hotmail.fr>
2022-02-01 08:55:44 +0100

    Entity Summoner

---
+++ src/Entity/Summoner.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SummonerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SummonerRepository::class)
+ */
+class Summoner
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=150)
+     */
+    private string $summonerId;
+
+    /**
+     * @ORM\Column(type="string", length=150)
+     */
+    private string $accountId;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $puuid;
+
+    /**
+     * @ORM\Column(type="string", length=150)
+     */
+    private string $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private int $profileIconId;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private \DateTimeImmutable $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private \DateTimeImmutable $updatedAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSummonerId(): ?string
+    {
+        return $this->summonerId;
+    }
+
+    public function setSummonerId(string $summonerId): self
+    {
+        $this->summonerId = $summonerId;
+
+        return $this;
+    }
+
+    public function getAccountId(): ?string
+    {
+        return $this->accountId;
+    }
+
+    public function setAccountId(string $accountId): self
+    {
+        $this->accountId = $accountId;
+
+        return $this;
+    }
+
+    public function getPuuid(): ?string
+    {
+        return $this->puuid;
+    }
+
+    public function setPuuid(string $puuid): self
+    {
+        $this->puuid = $puuid;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getProfileIconId(): ?int
+    {
+        return $this->profileIconId;
+    }
+
+    public function setProfileIconId(int $profileIconId): self
+    {
+        $this->profileIconId = $profileIconId;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
