f9613dc2b03561b334f1dcb73b96f783048051d0
Lilian <lilian.dorazio@hotmail.fr>
2022-02-04 16:35:42 +0100

    jquery + bootstrap 5

--- assets/app.js
+++ assets/app.js
@@ -3,3 +3,11 @@ import './styles/app.css';
 
 // start the Stimulus application
 //import './bootstrap';
+
+const $ = require('jquery');
+
+// $(".alert").show("slow").delay(1500).hide("slow");
+
+$(".alert-danger").fadeTo(2000, 500).slideUp(500, function() {
+    $(".alert-danger").slideUp(500);
+});
